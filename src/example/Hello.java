b08873bf7f703c658f097167822e50332548c434
Di Martino Lucas <lucas.di_martino@insa-cvl.fr>
2017-04-12 15:11:01 +0200

    Hello.java

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -1,21 +1,44 @@
-
 package example;
 
+import java.io.*;
+import java.util.*;
+import com.opencsv.*;
 import org.apache.commons.lang.WordUtils;
 
+
 public class Hello {
+
+    public static int max(int a, int b)
+    {
+    	return a > b ? a : b;
+    }
+
     public static void main(String[] args) {
         String  message = "hello ivy !";
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
-        int monmax = 0;
-        CSVReader reader = new CSVReader(new FileReader("data.csv"));
-        List<String[]> myEntries = reader.readAll();
-        for(String entry : myEntries)
+	
+	try {
+    	CSVReader reader = new CSVReader(new FileReader("data.csv"),',');
+    	List<String[]> myEntries = reader.readAll();
+	int monmax = 0;        
+	for(String[] s : myEntries)
         {
-            System.out.println("Nombre lu : " + entry);
+		for(String nb : s)
+		{
+			System.out.println("Nombre lu : "+ nb);
+                	monmax = max(monmax, Integer.parseInt(nb));
+
+		}
         }
+        System.out.println("Mon max : " + monmax);
+	
+	}
+	catch(Exception e)
+	{
+		System.out.println("Erreu" + e.getMessage());
+	}
     }
 }
         
